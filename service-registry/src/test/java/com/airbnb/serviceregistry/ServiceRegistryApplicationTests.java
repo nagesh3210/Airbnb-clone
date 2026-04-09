144b99168ae2df3754bc5a7c6644a0347078c28b
unknown <nagesh@gmail.com>
2026-04-09 23:34:29 +0530

    first commit

---
+++ service-registry/src/test/java/com/airbnb/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnb.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
